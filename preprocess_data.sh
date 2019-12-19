ff25d029e9365be598314cbf4b7af54e69c1937d
Guangli Dai <gdai@uh.edu>
2019-12-19 14:20:54 -0600

    Add the invoke of gantt chart drawing program

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -1,4 +1,3 @@
-#This file formats the target FILE_NAME into a gantt chart format.
 #this preprocess targets at domain 2,3's vcpu 0, going to make this a user-defined value later
 FILE_NAME=new_xentrace_aaf.txt
 RAW_FILE_NAME=raw_xentrace_aaf.txt
@@ -7,3 +6,5 @@ grep -E "[2-3]v0" ${RAW_FILE_NAME} > ${FILE_NAME}
 uniq ${FILE_NAME} > ${TMP}
 awk '{a[NR]=$1;b[NR]=$2} END{for(i=1;i<NR;i++) print a[i],b[i],b[i+1]}' ${TMP} > ${FILE_NAME}
 rm ${TMP}
+#To draw the gantt graph requires the installation of python and the matplotlib and numpy for that corresppponding version
+python3 draw_gantt_graph.py ${FILE_NAME}
